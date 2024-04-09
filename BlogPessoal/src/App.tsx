fced340bc02e69283b4809300cb41b8886a9eb7d
Gstalva <gstalva1@gmail.com>
2024-04-09 17:29:25 -0300

    hooks UseState UseEffect

--- BlogPessoal/src/App.tsx
+++ BlogPessoal/src/App.tsx
@@ -1,19 +1,13 @@
-import React, {useState} from 'react';
+import Home from './pages/home/Home';
 import './App.css';
-
+import React from 'react';
 
 function App() {
-  const [valor, setValor] = useState(0);
-  function handleClick(){
-    setValor(valor +1);
-  }
+ 
   return (
-    <div>
-      <h1>App</h1>
-      <p>O Valor é: {valor}</p>
-      <button onClick={handleClick}>Adicionar 1</button>
-
-    </div>
+    <>
+      <Home/>
+    </>
   );
 }
 
