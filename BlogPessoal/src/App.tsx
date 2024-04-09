83b394502089d849c9bd04c0e5dcce5a6813c1a8
Gstalva <gstalva1@gmail.com>
2024-04-09 17:19:04 -0300

    hook

--- BlogPessoal/src/App.tsx
+++ BlogPessoal/src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import Home from './pages/home/Home';
+import React, {useState} from 'react';
 import './App.css';
 
 
 function App() {
+  const [valor, setValor] = useState(0);
+  function handleClick(){
+    setValor(valor +1);
+  }
   return (
-   <Home  title="Componente Home"
-          description="Este é um componente Home que recebe props."/>
+    <div>
+      <h1>App</h1>
+      <p>O Valor é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+
+    </div>
   );
 }
 
