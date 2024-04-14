9dcec246f0d1f8c4f29f38737ea190862ea53b62
Gstalva <gstalva1@gmail.com>
2024-04-14 12:12:47 -0300

    cadastro login e rotas

--- BlogPessoal/src/App.tsx
+++ BlogPessoal/src/App.tsx
@@ -1,14 +1,32 @@
-import Home from './pages/home/Home';
-import './App.css';
 import React from 'react';
+import './App.css';
+
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+
 
 function App() {
- 
   return (
     <>
-      <Home/>
+   
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+  
     </>
   );
 }
-
 export default App;
